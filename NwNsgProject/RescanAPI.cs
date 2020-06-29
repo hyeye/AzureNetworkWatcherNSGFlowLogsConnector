d55dfbff47614562253d321728e7d53f4a9fce3a
hyeye <48680922+hyeye@users.noreply.github.com>
2020-06-29 17:11:10 +0900

    Update RescanAPI.cs

--- NwNsgProject/RescanAPI.cs
+++ NwNsgProject/RescanAPI.cs
@@ -103,41 +103,6 @@ namespace nsgFunc
             return (ActionResult)new OkObjectResult($"NSG flow logs for {blobName} were requested.");
 
         }
-        
-        class ThrowTest3
-{
-    static void ProcessString(string s)
-    {
-        if (s == null)
-        {
-            throw new ArgumentNullException();
-        }
-    }
-
-    static void Main()
-    {
-        try
-        {
-            string s = null;
-            ProcessString(s);
-        }
-        // Most specific:
-        catch (ArgumentNullException e)
-        {
-            Console.WriteLine("{0} First exception caught.", e);
-        }
-        // Least specific:
-        catch (Exception e)
-        {
-            Console.WriteLine("{0} Second exception caught.", e);
-        }
-    }
-}
-/*
- Output:
- System.ArgumentNullException: Value cannot be null.
- at Test.ThrowTest3.ProcessString(String s) ... First exception caught.
-*/
     }
 }
 
