266ad82633cdd33643ad0396cdbbb1bc4520f392
hyeye <48680922+hyeye@users.noreply.github.com>
2020-06-29 17:10:17 +0900

    Update Util.cs

--- NwNsgProject/Util.cs
+++ NwNsgProject/Util.cs
@@ -403,43 +403,7 @@ namespace nsgFunc
         static string kvp(bool firstOne, string key, string value)
         {
             return eqs(key, true) + eqs(value);
-        }
-        
-        class ThrowTest4
-{
-    static void ProcessString(string s)
-    {
-        if (s == null)
-        {
-            throw new ArgumentNullException();
-        }
-    }
-
-    static void Main()
-    {
-        try
-        {
-            string s = null;
-            ProcessString(s);
-        }
-        // Most specific:
-        catch (ArgumentNullException e)
-        {
-            Console.WriteLine("{0} First exception caught.", e);
-        }
-        // Least specific:
-        catch (Exception e)
-        {
-            Console.WriteLine("{0} Second exception caught.", e);
-        }
-    }
-}
-/*
- Output:
- System.ArgumentNullException: Value cannot be null.
- at Test.ThrowTest3.ProcessString(String s) ... First exception caught.
-*/
-        
+        }      
         
     }
 }
