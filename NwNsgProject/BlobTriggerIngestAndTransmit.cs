644df492af00bb4bf0e3ff9f3f40da94d06afa2b
hyeye <48680922+hyeye@users.noreply.github.com>
2020-06-29 10:28:28 +0900

    Update BlobTriggerIngestAndTransmit.cs

--- NwNsgProject/BlobTriggerIngestAndTransmit.cs
+++ NwNsgProject/BlobTriggerIngestAndTransmit.cs
@@ -115,40 +115,4 @@ namespace nsgFunc
             checkpoint.PutCheckpoint(checkpointTable, blockList.Count()-1);
         }
         
-        class ThrowTest1
-{
-    static void ProcessString(string s)
-    {
-        if (s == null)
-        {
-            throw new ArgumentNullException();
-        }
-    }
 
-    static void Main()
-    {
-        try
-        {
-            string s = null;
-            ProcessString(s);
-        }
-        // Most specific:
-        catch (ArgumentNullException e)
-        {
-            Console.WriteLine("{0} First exception caught.", e);
-        }
-        // Least specific:
-        catch (Exception e)
-        {
-            Console.WriteLine("{0} Second exception caught.", e);
-        }
-    }
-}
-/*
- Output:
- System.ArgumentNullException: Value cannot be null.
- at Test.ThrowTest3.ProcessString(String s) ... First exception caught.
-*/
-        
-    }
-}
