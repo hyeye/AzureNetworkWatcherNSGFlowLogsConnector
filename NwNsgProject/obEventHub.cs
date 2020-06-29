313e9b3dfc76a40f20eadbfa82679c4b1301e9dd
hyeye <48680922+hyeye@users.noreply.github.com>
2020-06-29 17:03:46 +0900

    Update obEventHub.cs

--- NwNsgProject/obEventHub.cs
+++ NwNsgProject/obEventHub.cs
@@ -14,18 +14,25 @@ namespace nsgFunc
     {
         private static Lazy<EventHubClient> LazyEventHubConnection = new Lazy<EventHubClient>(() =>
         {
-            try {
             string EventHubConnectionString = GetEnvironmentVariable("eventHubConnection");
             string EventHubName = GetEnvironmentVariable("eventHubName");
-
+            try 
+            {
             var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
             {
                 EntityPath = EventHubName
             };
-            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
             }
             catch (Exception e) {
                 Console.WriteLine("{0} evh 1 error", e);
+            }
+            
+            try
+            {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+            }
+            catch (Exception e) {
+                Console.WriteLine("{0} evh 2 error", e);
             }
 
             return eventHubClient;
@@ -33,19 +40,29 @@ namespace nsgFunc
 
         public static async Task<int> obEventHub(string newClientContent, ILogger log)
         {
+            
             var eventHubClient = LazyEventHubConnection.Value;
             int bytesSent = 0;
 
             foreach (var bundleOfMessages in bundleMessageListsJson(newClientContent, log))
             {
+                try 
+                {
                 await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(bundleOfMessages)));
                 bytesSent += bundleOfMessages.Length;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("{0} evh 3 error", e);
+                }
             }
+            
             return bytesSent;
         }
 
         static System.Collections.Generic.IEnumerable<string> bundleMessageListsJson(string newClientContent, ILogger log)
         {
+            try 
+            {
             foreach (var messageList in denormalizedRecords(newClientContent, null, log))
             {
                 var outgoingRecords = new OutgoingRecords();
@@ -58,6 +75,9 @@ namespace nsgFunc
 
                 yield return outgoingJson;
             }
+            } catch (Exception e) {
+                Console.WriteLine("{0} evh 4 error", e); 
+                                  }
         }
         
     }
