37651a85e17612c047f921460078d62068fcc0fb
hyeye <48680922+hyeye@users.noreply.github.com>
2020-06-29 17:09:45 +0900

    Update classes.cs

--- NwNsgProject/classes.cs
+++ NwNsgProject/classes.cs
@@ -472,37 +472,3 @@ class NSGFlowLogRecords
     public NSGFlowLogRecord[] records { get; set; }
 }
 
-class ThrowTest5
-{
-    static void ProcessString(string s)
-    {
-        if (s == null)
-        {
-            throw new ArgumentNullException();
-        }
-    }
-
-    static void Main()
-    {
-        try
-        {
-            string s = null;
-            ProcessString(s);
-        }
-        // Most specific:
-        catch (ArgumentNullException e)
-        {
-            Console.WriteLine("{0} First exception caught.", e);
-        }
-        // Least specific:
-        catch (Exception e)
-        {
-            Console.WriteLine("{0} Second exception caught.", e);
-        }
-    }
-}
-/*
- Output:
- System.ArgumentNullException: Value cannot be null.
- at Test.ThrowTest3.ProcessString(String s) ... First exception caught.
-*/
